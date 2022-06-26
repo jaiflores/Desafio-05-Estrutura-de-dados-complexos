c09c6d5fe6381a10386d33c8bcb1edaec6ac3f86
jaiflores <jaianny.vivian@gmail.com>
2022-06-26 16:36:58 -0300

    Desafio 05

---
+++ script.js
@@ -0,0 +1,29 @@
+// Crie um array que receba 5 itens e exiba no console.
+let fazenda = ["boi", "vaca", "árvore", "cavalo", "curral"];
+console.log(fazenda);
+
+
+// Utilize um método para adicionar um nome ao inicio do array.
+ const novoNaFazenda = fazenda.unshift("Bode");
+ console.log(fazenda);
+
+
+// // Utilize um método para remover o último nome do array.
+
+ const vendido = fazenda.pop();
+ console.log(fazenda);
+
+// // Utilize um método para adicionar dois nomes ao fim do array.
+ const compraNova = fazenda.push("cachorro", "égua");
+ console.log(fazenda);
+
+// Utilize um método para remover o primeiro nome do array.
+
+const perdeu = fazenda.shift();
+console.log(fazenda);
+
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+let numbers = [7,5,6,3,8,9,2,1,4];
+console.log(numbers.sort());
+
